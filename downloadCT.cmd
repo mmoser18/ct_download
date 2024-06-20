d8b2a1df4d5450994f1e941c0352b53a229ae5bd
Michael Moser <17732576+mmoser18@users.noreply.github.com>
2024-06-20 10:46:19 +0200

    fixed dashes in cmd-file

--- downloadCT.cmd
+++ downloadCT.cmd
@@ -4,5 +4,8 @@ set jar=%loc%\ct_download-1.0.0.jar
 cd %loc%
 rem Supported place-holders for the target-folder and download-folder strings are:
 rem %1$s: jahrgang, %2$s: last two digits of jahrgang, %3$s: issue-nr.
-java -jar "%jar%" -username "<username here>" -password "<password here>" -target-folder "<target folder path here>"
+rem with short options:
+rem java -jar "%jar%" -u "<username here>" -p "<password here>" -t "<target folder path here>"
+rem with spelled-out options:
+java -jar "%jar%" --username "<username here>" --password "<password here>" --target-folder "<target folder path here>"
 pause
